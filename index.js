c773a0e5cd9c92b42ce200c630dcdeaeb6ecaec0
Franklyn Colon <franklyncolon6@gmail.com>
2021-09-29 12:31:40 -0400

    commit tree 6cb1c8922943e7b4428bba64e3da54ffe123357f

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function saturdayFun(activity = "roller-skate!") {
-  return `This Saturday, I want to ${activity}`;
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
-saturdayFun();
-saturdayFun("bathe my dog!");
-const mondayWork = function () {
-  return "This Monday, I will go to the office.";
+const mondayWork = function (work = "go to the office") {
+  return `This Monday, I will ${work}.`;
 };
+function wrapAdjective(_initialized = "*") {
+  return function (string) {
+    return `You are ${_initialized}${string}${_initialized}!`;
+  };
+  //let result = wrapAdjective("||")
+  //let emphatic = result("a dedicated programmer")
+  // expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+}
