52a52c2cda9d70c5fde8a3f33f3def0e05124d8e
Franklyn Colon <franklyncolon6@gmail.com>
2021-09-29 11:27:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+function saturdayFun(activity = "roller-skate!") {
+  return `This Saturday, I want to ${activity}`;
+}
+saturdayFun();
+saturdayFun("bathe my dog!");
+const mondayWork = function () {
+  return "This Monday, I will go to the office.";
+};
